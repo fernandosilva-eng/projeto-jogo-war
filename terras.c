b42a011bd1c3d6501f0998ff76ab0b65f60158ba
fernandosilva-eng <fs2347554@gmail.com>
2025-09-08 21:34:25 -0300

    Update terras.c
    Mudei nomes

--- terras.c
+++ terras.c
@@ -6,48 +6,52 @@
 #define TAM_COR 10
 #define QUANT_TERR 5
 
-struct Territorio{
+struct Territorio
+{
     char nome[TAM_NOME];
     char cor[TAM_COR];
     int tropas;
 };
 
-void limparBuffer(){
+void limparBuffer()
+{
     int c;
-    while((c=getchar())!='\n'&&c!=EOF){}
+    while ((c = getchar()) != '\n' && c != EOF)
+    {
+    }
 }
 
-int main(){
+int main()
+{
 
     struct Territorio terras[QUANT_TERR];
 
     printf("\n\n=========================================================\n\n");
     printf("Vamos cadastrar os %d Territorios iniciais do nosso mundo.\n", QUANT_TERR);
 
-    for(int i=0; i<QUANT_TERR; i++){
-        printf("\n--- Cadastrando Territorio %d ---\n\n", i+1);
+    for (int i = 0; i < QUANT_TERR; i++)
+    {
+        printf("\n--- Cadastrando Territorio %d ---\n\n", i + 1);
         printf("Nome do Territorio: ");
         fgets(terras[i].nome, TAM_NOME, stdin);
         printf("Cor do Exercito (Ex: Azul, Verde, Vermelho...): ");
         fgets(terras[i].cor, TAM_COR, stdin);
 
-        terras[i].nome[strcspn(terras[i].nome, "\n")]='\0';
-        terras[i].cor[strcspn(terras[i].cor, "\n")]='\0';
+        terras[i].nome[strcspn(terras[i].nome, "\n")] = '\0';
+        terras[i].cor[strcspn(terras[i].cor, "\n")] = '\0';
 
         printf("Quantida de Tropas: ");
         scanf("%d", &terras[i].tropas);
         limparBuffer();
-
     }
 
     printf("\n\nCadastro concluido com sucesso!\n\n");
     printf("========================================\n      Mapa do Mundo - Estado Atual\n========================================\n\n");
-    for(int i=0; i<QUANT_TERR; i++){
-        printf("Territorio %d\n", i+1);
-        printf(" -Nome: %s\n", terras[i].nome);
-        printf(" -Dominado por: Exercito %s\n", terras[i].nome);
-        printf(" -Tropas: %d\n", terras[i].tropas);
+    for (int i = 0; i < QUANT_TERR; i++)
+    {
+        printf("TERRITORIO %d:\n", i + 1);
+        printf("  -Nome: %s\n", terras[i].nome);
+        printf("  -Dominado por: Exercito %s\n", terras[i].cor);
+        printf("  -Tropas: %d\n", terras[i].tropas);
     }
 }
-
-
